3afd61845480e82dd45dc8eb30342eb2bf007e41
Arash <47273556+arashnrim@users.noreply.github.com>
2019-07-05 21:33:09 +0800

    FEAT: Partially add InitialViewController
    In this commit, InitialViewController has been added. The UI has been
    fully set up while the code has been partially set up; code has been
    commented and set aside to prevent any Signal Abort errors.
    
    Besides adding InitialViewController into the project, the Metropolis
    font - which is used in the app - has been added into the project folder
    and assets supporting InitialViewController has also been added into the
    project. Firebase configuration is done in this commit, so it is critical
    to pull this project before executing the application.
    
    In future commits, InitialViewController will act as a redirection hub
    which verifies the current user status and performs redirection based on
    the returned value.

--- Upgrade/Upgrade/Storyboards/Main/Initial/InitialViewController.swift
+++ Upgrade/Upgrade/Storyboards/Main/Initial/InitialViewController.swift
@@ -1,10 +1,35 @@
 import UIKit
+import Firebase
 
 class InitialViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    // MARK: - Overrides
+    /// Configures status bar color;
+    /// changes color from black (default) to white for better readability
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        /// Prints out line to command for better debug purposes
+        print("upgradeconsoleSTART: Console line started.")
+        
+        /// Calls forward Firebase Auth to verify currentUser state
+        /// If:
+        /// a. user != nil (user is signed in), redirect to HomeViewController
+        /// b. user == nil (user not signed in), redirect to WelcomeViewController
+        let user = Auth.auth().currentUser
+        
+        // TODO: - Create HomeViewController and WelcomeViewController
+        // Code has been commented to prevent crashes; please remove this when implementation is done.
+        if user != nil {
+            print("upgradeconsoleAUTH: User is signed in, redirecting to HomeViewController...")
+            // performSegue(withIdentifier: "signedIn", sender: nil)
+        } else {
+            print("upgradeconsoleAUTH: User is not signed in, redirecting to WelcomeViewController...")
+            // performSegue(withIdentifier: "signedOut", sender: nil)
+        }
+        
     }
 
 
