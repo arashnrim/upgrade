e5906fdd428e2f8f014531cb05a43f352fe8f7cd
Arash <47273556+arashnrim@users.noreply.github.com>
2019-07-05 21:33:09 +0800

    FEAT: Partially add InitialViewController
    In this commit, InitialViewController has been added. The UI has been
    fully set up while the code has been partially set up; code has been
    commented and set aside to prevent any Signal Abort errors.
    
    Besides adding InitialViewController into the project, the Metropolis
    font - which is used in the app - has been added into the project folder
    and assets supporting InitialViewController has also been added into the
    project. Firebase configuration is done in this commit, so it is critical
    to pull this project before executing the application.
    
    In future commits, InitialViewController will act as a redirection hub
    which verifies the current user status and performs redirection based on
    the returned value.

--- Upgrade/Upgrade/AppDelegate.swift
+++ Upgrade/Upgrade/AppDelegate.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -8,6 +9,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // Imports and configures the Firebase framework into the project.
+        FirebaseApp.configure()
         return true
     }
 
