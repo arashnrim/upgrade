ef1ac797235fc55db0e1c27d3be608b074aef5fd
Arash <47273556+arashnrim@users.noreply.github.com>
2019-07-07 09:32:24 +0800

    FEAT: Partially add HomeViewController
    In this commit, HomeViewController has been added. Both the UI and
    code have been fully set up. The segue from InitialViewController to
    HomeViewController has also been established, but currently nothing
    is displayed to the user in the main View.
    
    Besides adding HomeViewController into the project, certain assets
    found in Assets.xcassets has been added to include assets for
    SignUpViewController.
    
    There may be minor changes to code in other View Controllers, such as
    comment amendments or any other miscellaneous actions.
    
    In future commits, HomeViewController will be able to segue into
    a menu View Controller that manages all the redirection to different
    View Controllers.
    
    This commit resolves and fixes issue #2.

---
+++ Upgrade/Upgrade/Storyboards/Main/Home/HomeViewController.swift
@@ -0,0 +1,69 @@
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    // MARK: - Outlets
+    @IBOutlet var viewBar: UIView!
+    @IBOutlet var viewMain: UIView!
+    @IBOutlet var labelQuote: UILabel!
+    @IBOutlet var labelAuthor: UILabel!
+    
+    // MARK: - Properties
+    let quotes = Quotes()
+    var count = Int.random(in: 1...8)
+    
+    // MARK: - Overrides
+    /// Configures status bar color; changes color from black (default) to white for better readability
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        /// Prints out line to command for better debug purposes
+        print("upgradeconsoleREDIRECT: Redirection to WelcomeViewController executed.")
+
+        /// Calls extension function (see UIView+Design.swift) to configure background gradient color for HomeViewController
+        self.view.configureViewController(color1: "UP Purple", color2: "UP Blue")
+        
+        /// Calls struct Quotes to assign a random quote
+        labelQuote.text = quotes.quote[count]
+        labelAuthor.text = quotes.author[count]
+        
+        /// Configures viewMain to have cornerRadius of 20; some corners are then masked to retain original rectangle shape
+        viewMain.layer.cornerRadius = 20
+        viewMain.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        
+        /// Configures viewBar to have round corners
+        viewBar.layer.cornerRadius = viewBar.bounds.height/2
+        
+        /// Adds gesture recognizers to move viewMain up and down
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(swipeGestures(gesture:)))
+        swipeDown.direction = UISwipeGestureRecognizer.Direction.down
+        viewMain.addGestureRecognizer(swipeDown)
+        
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(swipeGestures(gesture:)))
+        swipeUp.direction = UISwipeGestureRecognizer.Direction.up
+        viewMain.addGestureRecognizer(swipeUp)
+        
+    }
+    
+    // MARK: - Functions
+    @objc func swipeGestures(gesture: UIGestureRecognizer) {
+        if let gesture = gesture as? UISwipeGestureRecognizer {
+            switch gesture.direction {
+            case UISwipeGestureRecognizer.Direction.down:
+                UIView.animate(withDuration: 0.5) {
+                    self.viewMain.transform = CGAffineTransform(translationX: 0, y: 600)
+                }
+            case UISwipeGestureRecognizer.Direction.up:
+                UIView.animate(withDuration: 0.5) {
+                    self.viewMain.transform = CGAffineTransform(translationX: 0, y: 0)
+                }
+            default:
+                break
+            }
+        }
+    }
+    
+}
---
+++ Upgrade/Upgrade/Storyboards/Main/Menu/MenuViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MenuViewController.swift
+//  Upgrade
+//
+//  Created by Arash Nur Iman on 07/07/2019.
+//  Copyright © 2019 discipuli. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
