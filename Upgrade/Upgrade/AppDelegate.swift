aa8e59519aeaf735a3154ba516f183eaa4cb1737
Arash Nur Iman <47273556+ArashNrIm@users.noreply.github.com>
2020-06-26 19:33:43 +0800

    Extend app support
    This commit extends app support to older versions before iOS 13. Future
    commits should ensure that app support is sufficient and precautions are
    in place.

--- Upgrade/Upgrade/AppDelegate.swift
+++ Upgrade/Upgrade/AppDelegate.swift
@@ -10,13 +10,15 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
+    @available(iOS 13.0, *)
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
