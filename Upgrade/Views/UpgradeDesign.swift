f78ad9e9f730bdffa5f493e6dcf93b7f0b866278
Arash <47273556+arashnrim@users.noreply.github.com>
2020-12-18 19:23:25 +0800

    🚚 Move files
    This commit moves files to new locations with adaptations of the MVC
    structure and other common Xcode project structures. Hopefully, this
    makes navigating through the project much easier.

--- Upgrade/Views/UpgradeDesign.swift
+++ Upgrade/Views/UpgradeDesign.swift
@@ -8,6 +8,18 @@
 
 import UIKit
 
+extension UIButton {
+
+    func applyButtonDesign() {
+        // Adds a cornered radius around the sides of the button.
+        self.layer.cornerRadius = 10.0
+
+        // Applies the "UP Purple" colour.
+        self.backgroundColor = UIColor(named: "UP Purple")!
+    }
+
+}
+
 extension UIView {
 
     @objc func applyHeaderDesign() {
